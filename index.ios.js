9ebd6f185716b1d50e4e378e0f5bc1b2fb639f6d
ysy <1156546473@qq.com>
2016-07-19 00:13:56 +0800

    add Navigator

--- index.ios.js
+++ index.ios.js
@@ -3,9 +3,28 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+import React, {Component} from 'react'
+import {AppRegistry, Navigator} from 'react-native'
 
-import {AppRegistry,} from 'react-native';
+import PathTabBar from './app/src/script/tabBar/PathTabBar'
+import NavAllDay from './app/src/script/test/testNavigator'
 
-import PathTabBar from './app/src/script/tabBar/PathTabBar';
+class Path extends Component {
+    _renderScene(route, navigators) {
+        console.log(route, navigators)
+        return (
+            <PathTabBar />
+        )
+    }
+    render() {
+        return (
+            <Navigator
+                initialRoute={{ident: 'Myself'}}
+                ref="myselfNavgitor"
+                renderScene={this._renderScene}
+            />
+        )
+    }
+}
 
-AppRegistry.registerComponent('Path', () => PathTabBar);
+AppRegistry.registerComponent('Path', () => NavAllDay)
