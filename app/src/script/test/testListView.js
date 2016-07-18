5b7c065f4785f440c9c4bad1af46de9c231bbb9e
ysy <1156546473@qq.com>
2016-07-18 14:43:04 +0800

    test failed

--- app/src/script/test/testListView.js
+++ app/src/script/test/testListView.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {ListView, View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import {ListView, View, Text, StyleSheet, TouchableOpacity, TouchableHighlight} from 'react-native'
 
 export default class TestListView extends Component {
     constructor(props) {
@@ -11,19 +11,44 @@ export default class TestListView extends Component {
           ])
         };
     }
+    _renderSeperator(sectionID: number, rowID: number, adjacentRowHighlighted: bool) {
+        return (
+          <View
+            key={`${sectionID}-${rowID}`}
+            style={{
+              height: adjacentRowHighlighted ? 1 : 1,
+              backgroundColor: adjacentRowHighlighted ? '#f8f8f8' : '#eee',
+            }}
+          />
+        );
+    }
+    _renderRow(rowData: string, sectionID: number, rowID: number,
+        highlightRow: (sectionID: number, rowID: number) => void ) {
+            return (
+                <TouchableHighlight
+                activeOpacity={0.2}
+                underlayColor='#eee'
+                onPress = {() => {
+                    // highlightRow(sectionID, rowID);
+                }}>
+
+                    <View style={{ flex: 1, flexDirection: 'row'}}>
+                        <View style={{backgroundColor: 'white', width: 50, height: 60}}>
+                            <Text>
+                            2
+                            </Text>
+                        </View>
+                        <Text>{rowData}</Text>
+                    </View>
+                </TouchableHighlight>
+            )
+    }
     render() {
         return (
             <View style={style.container}>
                 <ListView dataSource={this.state.dataSource}
-                    renderRow={(rowData) => {
-                            return (
-                                <TouchableOpacity>
-                                    <View style={{borderBottomWidth: 1, borderBottomColor: '#eee', paddingTop: 20, paddingBottom: 20}}>
-                                        <Text>{rowData}</Text>
-                                    </View>
-                                </TouchableOpacity>
-                            )
-                    }}
+                    renderRow={this._renderRow}
+                    renderSeparator={this._renderSeperator}
                 />
             </View>
         )
@@ -32,6 +57,7 @@ export default class TestListView extends Component {
 
 var style = StyleSheet.create({
     container: {
-        paddingTop: 22
+        paddingTop: 22,
+        backgroundColor: '#f8f8f8'
     }
 });
